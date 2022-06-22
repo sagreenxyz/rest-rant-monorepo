233de15a025a4bccc04abfedf1b65c8611f93b88
Scott A. Green <sagreenxyz@gmail.com>
2022-06-22 14:58:14 -0500

    throw an error if attempting to comment without being logged in

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -118,6 +118,12 @@ router.post('/:placeId/comments', async (req, res) => {
         res.status(404).json({ message: `Could not find author with id "${req.body.authorId}"` })
     }
 
+    if (!currentUser) {
+        return res.status(404).json({
+            message: 'You must be logged in to leave a rant or rave.'
+        })
+    }
+
     const comment = await Comment.create({
         ...req.body,
         authorId: currentUser.userId,
