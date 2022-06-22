1c9a3f922d9678adee0057eaa8f78bc8e0d9c603
Scott A. Green <sagreenxyz@gmail.com>
2022-06-21 20:49:29 -0500

    configuring jwt and encoding against process.env.JWT_SECRET

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -16,7 +17,8 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password`
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId})
+        res.json({ user: user, token: result.value })
     }
 })
 
